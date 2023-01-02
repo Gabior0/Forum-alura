7d1d01de83d2a391ab6f63c3a12590a7c1f9b5cb
Gabior0 <ggoliveirarosa@gmail.com>
2023-01-01 23:27:20 -0300

    inclusão de testes automatizados

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -8,9 +8,4 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ForumApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
 }
